6fb291a663e9e328cac6748dcd802ad0783e5345
Rohan Varma <rvarm1@gmail.com>
2017-02-02 09:53:56 -0800

    finish MLE stuff

--- likelihood.py
+++ likelihood.py
@@ -1,13 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def plot(x, y):
-    plt.plot(x, y, 'x')
-    plt.axis('equal')
-    plt.show()
-
-
 def sample_from_bernoulli(p = 0.5):
     """Return 1 with probability p and 0 with probability 1 - p"""
     return 1 if np.random.rand() < p else 0
@@ -23,16 +16,48 @@ def compute_likelihood(data = [], p = 0.5):
         likelihood*=(p if x ==1 else 1 - p)
     return likelihood
 
+def get_max_val(d):
+    """ return tuple of max val in dict, assumes vals are >= 0"""
+    max_val = (-1, -1)
+    for k, v in d.items():
+        if v > max_val[1]:
+            max_val = k, v
+    return max_val
+
+def generate_likelihood(data, theta):
+    likelihood = [compute_likelihood(data, t) for t in theta]
+    likelihood_dict = dict(zip(theta, likelihood))
+    max_val = get_max_val(likelihood_dict)
+    return likelihood, likelihood_dict, max_val
+
+
 if __name__ == '__main__':
     data = [1,1,1,1,1,1,0,0,0,0]
     theta = np.arange(0.0,1.01,0.01)
-    likelihood = [compute_likelihood(data, t) for t in theta]
-    likelihood_dict = dict(zip(theta, likelihood))
+    likelihood, likelihood_dict, max_val = generate_likelihood(data, theta)
+    plt.plot(theta, likelihood)
+    plt.show()
+    print "max for 6 ones: " + str(max_val)
 
-    max_val = (-1, -1)
-    for k, v in likelihood_dict.items():
-        if v > max_val[1]:
-            max_val = (k, v)
-    print max_val
-    # print likelihood
-    # plot(theta, likelihood)
+    # 111 00 plot
+
+    data = [1,1,1,0,0]
+    likelihood, likelihood_dict, max_val = generate_likelihood(data, theta)
+    plt.plot(theta, likelihood)
+    plt.show()
+    print "max for 3 ones" + str(max_val)
+
+    # 60 ones plot
+    data = np.ones(100)
+    data[60:] = 0
+    likelihood, likelihood_dict, max_val = generate_likelihood(data, theta)
+    plt.plot(theta, likelihood)
+    plt.show()
+    print "Max for 60 ones: " + str(max_val)
+
+    # 5 ones plot
+    data = [1,1,1,1,1,0,0,0,0,0]
+    likelihood, likelihood_dict, max_val = generate_likelihood(data, theta)
+    plt.plot(theta, likelihood)
+    plt.show()
+    print "max for 5 ones: " + str(max_val)
