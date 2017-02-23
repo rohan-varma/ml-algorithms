b3360c1da35dfe9e5ffd23cc1fd8334aee960d5c
Rohan Varma <rvarm1@gmail.com>
2017-02-22 20:49:00 -0800

    linear regression gradient descent is buggy

--- linear_algorithms/linear_regression.py
+++ linear_algorithms/linear_regression.py
@@ -11,32 +11,54 @@ class LinearRegression(object):
     def __init__(self, dim = None):
         if dim:
             self.weights = np.random.random(dim)
+        else:
+            self.weights = None
 
-    def normalize(X):
-        return (X - np.mean(X, axis = 1)) /np.std(X, axis = 1)
+    def normalize(self, X):
+        return (X - np.mean(X, axis = 0)) /np.std(X, axis = 0)
 
-    def grad_desc_fit(X, y, max_iter = 1000, alpha = 0.01):
-        # normalize X
+    def grad_desc_fit(self, X, y, max_iter = 100000, alpha = 0.001, eps = 0.00001, verbose = True):
+        # make sure that x is normalized enough
+        mean, std = np.mean(X, axis = 0), np.std(X, axis = 0)
+        if mean.any() < -1 or mean.any() > 1 or std.any() > 2:
+            raise AssertionError("remember to normalize inputs!")
         if not self.weights or self.weights.shape[0] != X.shape[1]:
             self.weights = np.random.random(X.shape[1])
+        if verbose:
+            print "initial cost: " + str(self.cost(X,y))
+        prev_cost = self.cost(X,y)
         for i in range(max_iter):
-            h_theta = X.dot(self.weights)
-            grad = (h_theta - y).T.dot(X)
-            self.weights = self.weights - (alpha * grad)
+            X_T_X = X.T.dot(X)
+            t2 = X_T_X.dot(self.weights)
+            t3 = X.T.dot(y)
+
+            grad = t2 - t3
+            # print grad
+            self.weights+= -(alpha * grad)
+            new_cost = self.cost(X,y)
+            # if np.abs(new_cost - prev_cost) < eps:
+            #     break
+            # else:
+            #     prev_cost = new_cost
+        if verbose:
+            print "final cost, gradient descent: " + str(self.cost(X,y))
         return self.weights
 
 
-    def fit(X, y):
+    def fit(self, X, y, verbose = True):
         if not self.weights or self.weights.shape[0] != X.shape[1]:
             self.weights = np.random.random(X.shape[1])
+        print "initial cost: " + str(self.cost(X,y))
         self.weights = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(y)
+        print "final cost, explicit solution: " + str(self.cost(X,y))
+        print self.weights
         return self.weights
 
-    def predict(X):
+    def predict(self, X):
         return X.dot(self.weights)
 
-    def cost(X, y):
-        return np.dot((predict(X) - y).dot((predict(X) - y).T))
+    def cost(self, X, y):
+        return np.square(np.linalg.norm(self.predict(X)-y))/X.shape[0]
 
 
 if __name__ == '__main__':
@@ -49,3 +71,5 @@ if __name__ == '__main__':
     X = (X - np.mean(X, axis = 0))/np.std(X, axis = 0) # normalize
     X_train, X_test = X[:int(0.8 * X.shape[0])], X[int(0.8*X.shape[0]):]
     y_train, y_test = y[:int(0.8 * y.shape[0])], y[int(0.8*y.shape[0]):]
+    lr = LinearRegression()
+    lr.grad_desc_fit(X_train, y_train)
